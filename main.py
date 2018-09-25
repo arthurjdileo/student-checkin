d28a8b968e5f66325eeb87560251bc404170afe5
Arthur J. DiLeo <aj.dileo@gmail.com>
2018-09-25 08:43:34 -0400

    Added timestamp prelim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
+import datetime
 
-whitelist = [2019240]
+whitelist = [2019240, 2019291]
+ids = {2019240: "Arthur DiLeo Jr.", 2019291: "Nicholas Fierro"}
 
 timedIn = list()
 timedOut = list()
@@ -12,13 +13,12 @@ print("in " + str(timedIn))
 while True:
     id = input("Enter ID: ")
     id = int(id)
-    if id in whitelist and id in timedOut:
-        timedOut.remove(id)
-        timedIn.append(id)
-        print("out %s" % timedOut)
-        print("in %s" % timedIn)
-    elif id in whitelist and id in timedIn:
-        timedIn.remove(id)
-        timedOut.append(id)
-        print("out %s" % timedOut)
-        print("in %s" % timedIn)
+    if id in whitelist:
+        if id in timedOut:
+            timedOut.remove(id)
+            timedIn.append(id)
+            print("checked in %s at %s" % (ids[id], datetime.datetime.now()))
+        elif id in timedIn:
+            timedIn.remove(id)
+            timedOut.append(id)
+            print("checked out %s at %s" % (ids[id], datetime.datetime.now()))
